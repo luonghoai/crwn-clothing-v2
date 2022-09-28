d65bbfa59d804192ae77be9087e7a1bc24c89b25
luonghoai <nguyenluonghoai15@gmail.com>
2022-09-28 15:24:21 +0700

    commit sign in page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Navigation from "./routers/navigation/navagation.component";
 import Home from "./routers/home/home.component";
+import SignIn from "./routers/sign-in/sign-in.component";
 
 const Shop = () => {
   return <div>Shop Page</div>;
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
